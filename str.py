d889eafd7faa219804022bbbc369d1eb2ad4ddf8
Elifsu Karayiğit <karayigit.elifsu@std.izu.edu.tr>
2025-12-04 00:52:11 +0300

    python_average

---
+++ str.py
@@ -0,0 +1,24 @@
+"""
+
+kullanıcı_adı = input("Lütfen adınızı girin: ")
+kullanıcı_ps = int(input("Litfen şifre girininz: "))
+
+"""
+
+
+vize_puan = int(input("Lütfen vize notunu giriniz:"))
+final_puan = int(input("Lütfen final notunuzu girin:"))
+
+
+ortalama_hesapla = (final_puan + vize_puan)/2
+
+
+
+print(f"Vize puanınız:{vize_puan}\n Final puanınız{final_puan}\nOrtalamanız:{ortalama_hesapla}")
+
+
+# eğer tam sayı almak istersek 
+print("*"*10)
+print(f"Ortalamanız:{int(ortalama_hesapla)}")
+
+print("*"*10)
